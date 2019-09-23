0d63a01d065db9615b75eed89edbe281485884e6
BigGlebovsky <3696732@mail.ru>
2019-09-24 00:20:15 +0300

    make calc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,70 @@
+
 function eval() {
     // Do not use eval!!!
     return;
 }
 
+function calculateNumber(mark , expresion , index) {
+    let number;
+    if (mark === '/') {
+        number = expresion[index - 1] / expresion[index + 1];
+    }
+    if (mark === '*') {
+        number = expresion[index - 1] * expresion[index + 1];
+    }
+    if (mark === '+') {
+        number = +expresion[index - 1] + +expresion[index + 1];
+    }
+    if (mark === '-') {
+        number = +expresion[index - 1] - +expresion[index + 1];
+    }
+    return number;
+}
+
+function calculate(expr) {
+    const marksCalculation = ['/', '*', '-', '+'];
+    let expresion = expr;
+    marksCalculation.forEach(mark => {
+        let index = expresion.indexOf(mark);
+            while (index !== -1) {
+                let number = calculateNumber(mark, expresion, index);
+                expresion = [...expresion.slice(0, index - 1), number.toString(), ...expresion.slice(index + 2, expresion.length)];
+                index = expresion.indexOf(mark);
+            }
+        });
+    const [number] = expresion.filter((elem) => {
+        return !isNaN(parseInt(elem));
+    });
+    console.log(number);
+    return number;
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+    const rightBracket = ')';
+    const leftBracket = '(';
+    let exprArray = expr.split(' ');
+
+    let firstRightBracket = exprArray.indexOf(rightBracket);
+    let closeBracket = exprArray.lastIndexOf(leftBracket, firstRightBracket);
+
+    while (firstRightBracket !== -1) {
+        const expression = exprArray.slice(closeBracket, firstRightBracket + 1);
+        let number = calculate(expression);
+        exprArray = [...exprArray.slice(0, closeBracket), number, ...exprArray.slice(firstRightBracket + 1, exprArray.length)];
+        firstRightBracket = exprArray.indexOf(rightBracket);
+        closeBracket = exprArray.lastIndexOf(leftBracket, firstRightBracket);
+    }
+    console.log(exprArray);
+    exprArray = calculate(exprArray);
+
+    return parseFloat(parseFloat(exprArray).toFixed(4));
 }
 
 module.exports = {
     expressionCalculator
 }
+
+const expr = "2-2";
+
+"Nested brackets test 28"
+console.log(expressionCalculator(expr));
